afe11a7219f6d3aa6e27d5e11d88f5dd12c7c2e7
sesona297 <240773365@mycput.ac.za>
2026-07-22 17:58:53 +0200

    Initial Spring Boot project setup and package structure

---
+++ HospitalManagementSystem/src/test/java/za/ac/cput/hospitalmanagementsystem/HospitalManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package za.ac.cput.hospitalmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
